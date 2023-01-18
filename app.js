911b6c6a4ae71642fecfb9007cb2eabc185e58d3
Athirson <athirsonarceus@gmail.com>
2023-01-18 19:04:43 -0300

    random spawn point

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const mapData = {
+	minX: 1,
+	maxX: 14,
+	minY: 4,
+	maxY: 12,
+	blockedSpaces: {
+		'7x4': true,
+		'1x11': true,
+		'12x10': true,
+		'4x7': true,
+		'5x7': true,
+		'6x7': true,
+		'8x6': true,
+		'9x6': true,
+		'10x6': true,
+		'7x9': true,
+		'8x9': true,
+		'9x9': true
+	}
+}
+
 const playerColors = [
 	'black',
 	'blue',
@@ -59,6 +80,46 @@ const createName = () => {
 	return `${randomFromArray(adjectives)} ${randomFromArray(animals)}`
 }
 
+function isSolid(x, y) {
+	const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)]
+
+	return (
+		blockedNextSpace ||
+		x >= mapData.maxX ||
+		x < mapData.minX ||
+		y >= mapData.maxY ||
+		y < mapData.minY
+	)
+}
+
+function getRandomSafeSpot() {
+	return randomFromArray([
+		{ x: 1, y: 4 },
+		{ x: 2, y: 4 },
+		{ x: 1, y: 5 },
+		{ x: 2, y: 6 },
+		{ x: 2, y: 8 },
+		{ x: 2, y: 9 },
+		{ x: 4, y: 8 },
+		{ x: 5, y: 5 },
+		{ x: 5, y: 8 },
+		{ x: 5, y: 10 },
+		{ x: 5, y: 11 },
+		{ x: 11, y: 7 },
+		{ x: 12, y: 7 },
+		{ x: 13, y: 7 },
+		{ x: 13, y: 6 },
+		{ x: 13, y: 8 },
+		{ x: 7, y: 6 },
+		{ x: 7, y: 7 },
+		{ x: 7, y: 8 },
+		{ x: 8, y: 8 },
+		{ x: 10, y: 8 },
+		{ x: 8, y: 8 },
+		{ x: 11, y: 4 }
+	])
+}
+
 ;(() => {
 	let playerId
 	let playerRef
@@ -71,7 +132,7 @@ const createName = () => {
 		const newX = players[playerId].x + xChange
 		const newY = players[playerId].y + yChange
 
-		if (true) {
+		if (!isSolid(newX, newY)) {
 			players[playerId].x = newX
 			players[playerId].y = newY
 
@@ -152,22 +213,32 @@ const createName = () => {
 			characterElement.style.transform = `translate3d(${left}, ${top}, 0)`
 			gameContainer.appendChild(characterElement)
 		})
+
+		allPlayersRef.on('child_removed', (snapshot) => {
+			const removedKey = snapshot.val().id
+
+			gameContainer.removeChild(playerElements[removedKey])
+
+			delete playerElements[removedKey]
+		})
 	}
 
 	firebase.auth().onAuthStateChanged((user) => {
-		console.log(user)
 		if (user) {
 			// User is signed in.
 			playerId = user.uid
 			playerRef = firebase.database().ref(`players/${playerId}`)
 
+			const name = createName()
+			const { x, y } = getRandomSafeSpot()
+
 			playerRef.set({
 				id: playerId,
-				name: createName(),
+				name: name,
 				direction: 'left',
 				color: randomFromArray(playerColors),
-				x: 4,
-				y: 4,
+				x,
+				y,
 				coins: 0
 			})
 		}
